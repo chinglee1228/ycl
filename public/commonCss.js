bb24bb911e459346f3a988458b4e3a14c62b8aad
chinglee1228 <40107nh@gmail.com>
2024-02-22 00:53:14 +0800

    程式碼簡化整合

---
+++ public/commonCss.js
@@ -0,0 +1,7 @@
+//<!-- Vendor CSS Files -->
+document.write('<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">');
+document.write('<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">');
+document.write('<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">');
+document.write('<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">');
+document.write('<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">');
+
---
+++ public/commonjs.js
@@ -0,0 +1,11 @@
+//<!-- Vendor JS Files -->
+document.write('<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>');
+document.write('<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>');
+document.write('<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>');
+document.write('<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>');
+document.write('<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>');
+document.write('<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>');
+document.write('<script src="assets/vendor/php-email-form/validate.js"></script>');
+//<!-- Template Main JS File -->
+document.write('<script src="assets/js/main.js"></script>');
+
